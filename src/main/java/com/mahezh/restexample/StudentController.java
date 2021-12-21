05cc76defc5d6a7e290dbbc5fb89174ab7842af6
Mah-eshh <92861@fhss.sjp.ac.lk>
2021-12-21 21:00:56 +0530

    Add GET method to retrive a List of Students

--- src/main/java/com/mahezh/restexample/StudentController.java
+++ src/main/java/com/mahezh/restexample/StudentController.java
@@ -16,6 +16,17 @@ public class StudentController {
 		return new Student("Mahesh","Abeykoon");
 	}
 	
+	@GetMapping("/students")
+	public List<Student> getAllStudents(){
+		
+		List<Student> students = new ArrayList<>();
+		students.add(new Student("Mahesh1", "Abey1"));
+		students.add(new Student("Mahesh2", "Abey2"));
+		students.add(new Student("Mahesh3", "Abey3"));
+		students.add(new Student("Mahesh4", "Abey4"));
+		
+		return students;
+	}
 	
 	 
 }
