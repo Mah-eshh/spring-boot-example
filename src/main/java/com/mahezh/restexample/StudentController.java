f9127a1ac1929965c437a864051301f64cd75f9b
Mah-eshh <92861@fhss.sjp.ac.lk>
2021-12-21 22:03:33 +0530

    Create API to handle query paramters

--- src/main/java/com/mahezh/restexample/StudentController.java
+++ src/main/java/com/mahezh/restexample/StudentController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -35,4 +36,13 @@ public class StudentController {
 		return new Student(firstName, lastName);
 	}
 	 
+	//http://localhost:8080/student/query?firstName=Mahesh&lastName=Abeykoon
+	//API to handle query parameters
+	@GetMapping("/student/query")
+	public Student studentQueryParam(
+			@RequestParam(name ="firstName") String firstName, 
+			@RequestParam(name ="lastName")  String lastName) {
+		return new Student (firstName, lastName);
+		
+	}
 }
